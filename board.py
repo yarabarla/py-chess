1166a1dc220cd8d47bfba18364d25db3749696b8
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-18 17:38:28 -0500

    Set up __init__ to use Move's set_new_game method to create a new board

--- board.py
+++ board.py
@@ -1,9 +1,9 @@
+import Piece, Move
+
 class Board(object):
     """Manipulates the chess board"""
     def __init__(self):
+        """Calls create_board method to make the initialize the board when Board is instantiated"""
+        self.main_board = []
+        Move.set_new_game(self.main_board)
 
-        self.create_board()
-
-    def create_board(self):
-        """Creates a new chess board"""
-        

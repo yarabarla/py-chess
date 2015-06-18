86bfb9c88ea31ccaba096ff7f5339d42567b3d10
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-17 22:31:33 -0500

    Started writing the board class

---
+++ board.py
@@ -0,0 +1,10 @@
+class Board(object):
+    """Manipulates the chess board"""
+
+    def __init__(self):
+        
+        self.create_board()
+
+    def create_board(self): 
+        """Creates a new chess board"""
+        

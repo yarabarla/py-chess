a6a8deae0fca1a2e7e1fb5e6e9d4d1d5da7b77e0
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-18 18:22:11 -0500

    Overloaded __repr__ to display the formatted board

--- board.py
+++ board.py
@@ -7,3 +7,17 @@ class Board(object):
         self.main_board = []
         Move.set_new_game(self.main_board)
 
+    def __repr__(self):
+        """Formats how the board will be displayed. Displays rank and file letters/numbers and pieces"""
+        disp = "  1 2 3 4 5 6 7 8\n" # Displays 
+        for row in xrange(8):
+            disp += chr(ord('A') + row) + ' '
+            for col in xrange(8):
+                disp += str(self.main_board[row][col])
+
+            disp += '\n'
+            
+        return disp
+
+new = Board()
+print new

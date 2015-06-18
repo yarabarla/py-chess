8227eea34481fb302900222bfba29da9eea1dce9
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-17 22:50:10 -0500

    Changed __init__ spacing to match PEP 8

--- board.py
+++ board.py
@@ -1,10 +1,9 @@
 class Board(object):
     """Manipulates the chess board"""
-
     def __init__(self):
-        
+
         self.create_board()
 
-    def create_board(self): 
+    def create_board(self):
         """Creates a new chess board"""
         

0afa942a09b2c5eebd285299aa3bd9e634920cf2
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-18 22:17:45 -0500

    Added unicode __repr__ overloading for all pieces

--- Piece.py
+++ Piece.py
@@ -4,8 +4,12 @@ class Piece(object):
         self.empty = empty
         self.color = color
 
+    def __repr__(self):
+        if self.empty:
+            return u"\u25A1"
+
 class Pawn(Piece):
-    """Contains attributes of a pawn. Unicode for black and white pieces switched around since they are going to be displayed on a black background"""
+    """Contains attributes of a pawn. Unicode for all black and white pieces switched around since they are going to be displayed on a black background"""
     def __repr__(self):
         if self.color == "White":
             return u"\u265f"
@@ -15,22 +19,47 @@ class Pawn(Piece):
 
 class Rook(Piece):
     """Contains attributes of a rook"""
-    pass
+    def __repr__(self):
+        if self.color == "White":
+            return u"\u265C"
+
+        else:
+            return u"\u2656"
 
 class Knight(Piece):
     """Contains attributes of a knight"""
-    pass
+    def __repr__(self):
+        if self.color == "White":
+            return u"\u265E"
+
+        else:
+            return u"\u2658"
 
 class Bishop(Piece):
     """Contains attributes of a bishop"""
-    pass
+    def __repr__(self):
+        if self.color == "White":
+            return u"\u265D"
+
+        else:
+            return u"\u2657"
 
 class Queen(Piece):
     """Contains attributes of a queen"""
-    pass
+    def __repr__(self):
+        if self.color == "White":
+            return u"\u265B"
+
+        else:
+            return u"\u2655"
 
 class King(Piece):
     """Contains attributes of a king"""
-    pass
+    def __repr__(self):
+        if self.color == "White":
+            return u"\u265A"
+
+        else:
+            return u"\u2654"
 
 

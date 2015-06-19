8de9db41955ba000b6a97a0ccac86f4d5d8d6e72
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-18 19:48:34 -0500

    Finished writing set_new_game method

--- Move.py
+++ Move.py
@@ -2,15 +2,49 @@ import Piece
 
 def set_new_game(board):
     """Initializes pieces for a new chess game"""
-    A, B, C, D, E, F, G, H = [], [], [], [], [], [], [], []
+    A, B, C, D, E, F, G, H = range(8), range(8), range(8), range(8), range(8), range(8), range(8), range(8) # Feels unpythonic but can't do A = B = C = ... = range(8) since lists are mutable
     board.extend([A, B, C, D, E, F, G, H])
 
     for row in xrange(8):
-        for pe in xrange(1,7):
-            if pe == 1 or pe == 6:
-                board[row][pe] = Pawn()
+        for col in xrange(8):  # Columns 1 - 6 are all pawns and empty so this loop is to place them
+            if col == 1:
+                board[row][col] = Piece.Pawn(False, 'White')
 
+            elif col == 6:
+                board[row][col] = Piece.Pawn(False, 'Black')
 
-def test()
-    
+            elif col in range(2,7):
+                board[row][col] = Piece.Piece(True, 'N/A')
+
+            elif col == 0:
+                if row == 0 or row == 7:
+                    board[row][col] = Piece.Rook(False, 'White')
+
+                elif row == 1 or row == 6:
+                    board[row][col] = Piece.Knight(False, 'White')
+
+                elif row == 2 or row == 5:
+                    board[row][col] = Piece.Bishop(False, 'White')
+
+                elif row == 3:
+                    board[row][col] = Piece.Queen(False, 'White')
+
+                else:
+                    board[row][col] = Piece.King(False, 'White')
+
+            else:
+                if row == 0 or row == 7:
+                    board[row][col] = Piece.Rook(False, 'Black')
+
+                elif row == 1 or row == 6:
+                    board[row][col] = Piece.Knight(False, 'Black')
+
+                elif row == 2 or row == 5:
+                    board[row][col] = Piece.Bishop(False, 'Black')
+
+                elif row == 3:
+                    board[row][col] = Piece.Queen(False, 'Black')
+
+                else:
+                    board[row][col] = Piece.King(False, 'Black')
 

dcdd179857d9f7dcd18d04a83eb6f0e5581c78a4
Akhilesh Yarabarla <akhileshyarabarla@gmail.com>
2015-06-20 23:33:39 -0500

    Changed documentation spacing a bit

--- Piece.py
+++ Piece.py
@@ -1,5 +1,8 @@
 class Piece(object):
-    """Generalized piece class that contains attributes about whether it is an empty piece or if it is black or white. All pieces except empty have their own subclass. Contains a list of locations the piece can move to"""
+    """
+    Generalized piece class that contains attributes about whether it is an empty piece or if it is black or white.
+    All pieces except empty have their own subclass. Contains a list of locations the piece can move to
+    """
     def __init__(self, empty, color):
         self.empty = empty
         self.color = color
@@ -9,7 +12,10 @@ class Piece(object):
             return u"\u25A1"
 
 class Pawn(Piece):
-    """Contains attributes of a pawn. Unicode for all black and white pieces switched around since they are going to be displayed on a black background"""
+    """
+    Contains attributes of a pawn. Unicode for all black and white pieces switched around since they
+    are going to be displayed on a black background
+    """
     def __repr__(self):
         if self.color == "White":
             return u"\u265f"
